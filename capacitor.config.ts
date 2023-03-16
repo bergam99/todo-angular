136307d1b861885e9416689593920163a0088bb8
Jiyun <grosjeanjiyun@icloud.com>
2023-03-16 17:27:51 +0100

    creation de projet

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Todo-Angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
