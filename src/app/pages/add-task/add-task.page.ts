17c1f074c544af016f20077a5d2ec63fd2588059
Jiyun <grosjeanjiyun@icloud.com>
2023-03-23 21:26:57 +0100

    add fonctionne git commit -m marche git push origin work yeah

--- src/app/pages/add-task/add-task.page.ts
+++ src/app/pages/add-task/add-task.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { TodoService} from 'src/app/services/todo.service';
+import { TodoService } from 'src/app/services/todo.service';
+import { ITodo } from 'src/app/services/todo.service';
+import { CategoryType } from '../../services/todo.service';
 
 @Component({
   selector: 'app-add-task',
@@ -8,47 +10,117 @@ import { TodoService} from 'src/app/services/todo.service';
   styleUrls: ['./add-task.page.scss'],
 })
 export class AddTaskPage implements OnInit {
-  isShoppingSelected:boolean = false;
-  isInscureSelected:boolean = false;
-  isWorkSelected:boolean = false;
-  isBrokedSelected:boolean = false;
-  isCleanSelected:boolean = false;
-  isAnotherSelected:boolean = false;
+  @ViewChild('myForm') myForm: any;
+  categories = ['🛍️', '💊', '💼', '💸', '🧼', '🤷‍♀️'];
+  task: ITodo = {
+    id: 0,
+    content: '',
+    category: null,
+    isUrgent: false,
+    doneDate: null
+  };
 
+  constructor(private todoService: TodoService, private fb: FormBuilder) {}
+  ngOnInit() {}
 
-  newTask: string='';
-  registerationForm! : FormGroup;
-  task:any;
+  onSubmit(): void {
+    // Generate a unique ID for the task
+    const taskId = new Date().getTime();
 
-  constructor(
-    private todoService: TodoService,
-    private fb:FormBuilder
-    ) {
-      this.task = {};
-      // this.newTask = '';
+    // Set the ID and add the task to the service
+    this.task.id = taskId;
+    this.todoService.addTask(this.task);
+    this.myForm.resetForm();
 
-    }
-
-
-  ngOnInit() {
-    this.createRegisterationForm();
-  }
-
-  onSubmit() {
-    console.log(this.registerationForm);
-    this.task = Object.assign(this.task, this.registerationForm.value);
-    localStorage.setItem('task', JSON.stringify(this.task.input));
-    this.todoService.addTask(this.task.input);
-
-    // Reset the input value to an empty string
-    this.newTask = '';
-  }
-
-  createRegisterationForm(){
-    this.registerationForm = this.fb.group({
-      // type:[null, Validators.required],
-      input:[null, Validators.required],
-      urgent : [null, Validators.required]
-    })
   }
 }
+
+  // ======================
+  // isShoppingSelected: boolean = false;
+  // isInscureSelected: boolean = false;
+  // isWorkSelected: boolean = false;
+  // isBrokedSelected: boolean = false;
+  // isCleanSelected: boolean = false;
+  // isAnotherSelected: boolean = false;
+
+  // selectedIcon: string = '';
+  // categories: string[] = [
+  //   'category1',
+  //   'category2',
+  //   'category3',
+  //   'category4',
+  //   'category5',
+  //   'category6',
+  // ];
+
+  // newTask: string = '';
+  // registerationForm!: FormGroup;
+  // task: any;
+  // categoryForm!: FormGroup;
+
+  // constructor(private todoService: TodoService, private fb: FormBuilder) {
+  //   // propriété task est initialisée comme un objet vide. Cette propriété peut être utilisée pour stocker une tâche individuelle à ajouter à la liste de tâches.
+  //   this.task = {};
+  //   // this.newTask = '';
+
+  //   this.registerationForm = this.fb.group({
+  //     category: ['', Validators.required],
+  //     input: ['', Validators.required],
+  //     urgent: false,
+  //   });
+
+
+  // onSelect(icon: string): void {
+  //   this.selectedIcon = icon;
+  // }
+
+  // onSubmit1(): void {
+  //   localStorage.setItem('selectedIcon', JSON.stringify(this.selectedIcon));
+  // }
+
+  // ngOnInit() {
+  //   this.createRegisterationForm();
+  //   this.categoryForm = this.fb.group({
+  //     // category:[''],
+  //     category: ['', Validators.required],
+  //   });
+  // }
+
+  // onSubmit() {
+  //   console.log(this.registerationForm);
+  //   // this.task 객체에 this.registrationForm.value 객체를 병합(merge)합니다. 이렇게 하면 this.task 객체는 폼 요소의 값을 포함하게 됩니다.
+  //   this.task = Object.assign(this.task, this.registerationForm.value);
+  //   // localStorage에 this.task.input 값을 JSON 문자열로 저장합니다. 이렇게 하면 사용자가 페이지를 새로고침하거나 브라우저를 닫고 다시 열어도 이 값을 유지할 수 있습니다.
+  //   localStorage.setItem('task', JSON.stringify(this.task.input));
+  //   // this.todoService의 addTask 메서드를 호출하여 this.task.input 값을 서비스에 추가합니다.
+  //   this.todoService.addTask(this.task.input);
+
+  //   // Reset the input value to an empty string
+  //   this.newTask = '';
+
+  //   const task: ITodo = {
+  //     id: Date.now(),
+  //     content: this.registerationForm.value.content,
+  //     category: this.registerationForm.value.category,
+  //     isUrgent: this.registerationForm.value.isUrgent,
+  //     doneDate: null,
+  //   };
+  //   this.todoService.addTask(task);
+  // }
+
+  // // 메소드는 createRegistrationForm으로 정의되며, FormGroup 객체를 생성하고 해당 폼의 FormControl을 정의하는 데 사용됩니다. 이 메소드는 this.fb.group()을 사용하여 폼 그룹 객체를 생성합니다.
+  // // 폼 그룹 객체는 폼의 모든 컨트롤의 논리적 그룹입니다. 이 경우, registerationForm은 FormGroup 객체를 할당받아, 폼의 모든 컨트롤을 논리적으로 그룹화합니다.
+
+  // createRegisterationForm() {
+  //   this.registerationForm = this.fb.group({
+  //     // type:[null, Validators.required],
+  //     input: [null, Validators.required],
+  //     urgent: [null, Validators.required],
+  //   });
+  // }
+
+  // setCategory(category: string) {
+  //   this.categoryForm.controls['category'].setValue(category);
+  //   this.todoService.setCategory(category);
+  // }
+// }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoService } from 'src/app/services/todo.service';
+import { ITodo } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,19 @@ import { TodoService } from 'src/app/services/todo.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  tasks:string[]=[];
+  tasks:ITodo[]=[];
+  category:string='';
 
   constructor(private todoService:TodoService) { }
 
+
+// ngOnInit récupère la liste des tâches en appelant la méthode getTasks() du service.  Ensuite, la liste de tâches est assignée à la propriété tasks du composant.
   ngOnInit(){
     this.tasks = this.todoService.getTasks();
+
+    // this.tasks = this.todoService.getTasks();
+    // this.category = this.todoService.getCategory();
   }
+
+
 }
