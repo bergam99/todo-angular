85388442f4f49153b754c2d213db2a772c13c0dd
Jiyun <grosjeanjiyun@icloud.com>
2023-04-13 22:57:53 +0200

    scroll bug fixed git add . finishhh

--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -12,41 +12,33 @@ export class HistoryPage implements OnInit {
   doneTasks: ITodo[] = [];
   tasks: ITodo[] = [];
 
-
   constructor(private todoService: TodoService, private router: Router) {
     this.doneTasks = this.todoService.getDoneTasks();
-    // this.doneTasks = this.todoService.getDoneTasks().sort((a, b) => {
-    //   // Sort tasks in reverse chronological order based on doneDate
-    //   return new Date(b.doneDate).getTime() - new Date(a.doneDate).getTime();
-  };
+
+  }
 
   ngOnInit(): void {
     this.doneTasks = this.todoService.getDoneTasks();
     // console.log(this.doneTasks);
   }
 
-
   showAddTaskForm() {
     if (this.tasks.length === 0) {
       this.router.navigate(['/add-task']);
-    }}
-    onCheckboxClick(event: Event, task: ITodo) {
-      task.isComplete = !task.isComplete;
+    }
+  }
 
-      if (!task.isComplete) {
-        task.doneDate = null;
-      }
-          // Ajouter la tâche au tableau ades tâches terminées
-          this.todoService.doneTasks.push(task);
+  onCheckboxClick(event: Event, task: ITodo) {
+    task.isComplete = !task.isComplete;
 
-          // Enregistrer les modifications dans le stockage local
-          this.todoService.saveTasks();
-        }
+    if (!task.isComplete) {
+      task.doneDate = null;
+    }
+    // Ajouter la tâche au tableau ades tâches terminées
+    this.todoService.doneTasks.push(task);
 
-      }
-
-  // showAddTaskForm() {
-  //   if (this.tasks.length === 0) {
-  //     this.router.navigate(['/add-task']);
-  //   }}
+    // Enregistrer les modifications dans le stockage local
+    this.todoService.saveTasks();
+  }
+}
 
